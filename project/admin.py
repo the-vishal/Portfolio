04344b680ba394a730dea516e3676ad9e9ddfe8c
Vishal <mail007tovishal@gmail.com>
2020-01-11 23:01:19 +0530

    admin date filter + formatting

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Project
+from django.contrib.admin.filters  import DateFieldListFilter
 # Register your models here.
 
-admin.site.register(Project)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+	date_hierarchy = 'date'
+	list_filter = (	
+		('date', DateFieldListFilter),
+	)
+	list_display = ('name', 'date')
+	search_fields = ('name', )
+
+# admin.site.register(Project, ProjectAdmin)
--- project/models.py
+++ project/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 class Project(models.Model):
-	image = models.ImageField(upload_to='images/')
+	image = models.ImageField(upload_to='images/', default='images/default_project.gif')
 	name = models.CharField(max_length=50)
 	summary = models.CharField(max_length =200)
-	link = models.CharField(max_length=200, default=0)
-
+	link = models.CharField(max_length=200, default='#')
+	date = models.DateTimeField(default=datetime.now(), blank=True)
 	def __str__(self):
 		return self.name
